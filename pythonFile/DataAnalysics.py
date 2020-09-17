c180a0f1c1d3fdbc1922eb4fea01e38c6627a435
unknown <zhangkeke@jahwa.com.cn>
2020-09-17 17:06:09 +0800

    update

--- pythonFile/DataAnalysics.py
+++ pythonFile/DataAnalysics.py
@@ -9,3 +9,16 @@ sales['销售数量'] = sales['销售数量'].astype('float')
 sales['应收金额'] = sales['应收金额'].astype('float')
 sales['实收金额'] = sales['实收金额'].astype('float')
 print('after as type:', sales.dtypes)
+
+
+def  saleTimeTransfer(timeStr):
+    timeList = []
+    for value in timeStr:
+        dateStr = value.split(' ')[0]
+        timeList.append(dateStr)
+    timeSer = pd.Series(timeList)  # 将列表转行为一维数据Series类型
+    return timeSer
+
+    time= sales.loc[:,'购药时间']
+    newDate= saleTimeTransfer(time)
+    sales.loc[:,'销售时间'] = newDate
